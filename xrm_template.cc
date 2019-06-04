3e19845ffc7a7db9957118fc6e0849de087f8507
Lars H. Rohwedder <RokerHRO@users.noreply.github.com>
2019-06-04 12:04:30 +0200

    XRM as a class template

---
+++ xrm_template.cc
@@ -0,0 +1,12 @@
+template<uint64_t X, uint64_t R, uint64_t M>
+class XRM
+{
+    public:
+    uint64_t operator()(uint64_t value) const
+    {
+        value ^= X;
+        value = (value>>R) + (value<<(64-R));
+        value *= M;
+        return value;
+    }
+};
